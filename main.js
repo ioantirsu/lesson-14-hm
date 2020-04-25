4a50b5ae362f9585cdf4e9b6c65cc243d31726ae
Ioan <john.tirsu@yahoo.com>
2020-04-25 17:38:23 +0300

    Homework 14

---
+++ main.js
@@ -0,0 +1,23 @@
+
+/*
+let availableQuantity = 15;
+
+
+let answer = prompt('Please enter the Quanitity needed')
+
+if (answer > availableQuantity) {
+    console.log('Sorry we do not have enough in stock')
+
+}
+else {
+    console.log('Great, we will soon ship your order');
+}
+*/
+
+let answer = prompt('Please enter the Quanitity needed');
+
+let result = answer < 15 ? console.log('Great, we will soon ship your order') : console.log('Sorry we do not have enough in stock');
+
+
+
+
